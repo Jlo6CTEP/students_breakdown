98449a944afa9207305d9b2e1fb49c8de30fb2d1
HardLight00 <imazan00@gmail.com>
2019-03-19 09:45:33 +0300

    Working login & register

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,18 +1,19 @@
 <template>
     <div>
         <h1>Hi {{account.user.firstName}}!</h1>
-        <p>You're logged in with Vue + Vuex & JWT!!</p>
-        <h3>Users from secure api end point:</h3>
-        <em v-if="users.loading">Loading users...</em>
-        <span class="text-danger" v-if="users.error">ERROR: {{users.error}}</span>
-        <ul v-if="users.items">
-            <li :key="user.id" v-for="user in users.items">
-                {{user.firstName + ' ' + user.lastName}}
-                <span v-if="user.deleting"><em> - Deleting...</em></span>
+        <p>Surname: {{account.user.lastName}}</p>
+        <p>Email: {{account.user.username}}</p>
+        <!--<h3>Users from secure api end point:</h3>-->
+        <!--<em v-if="users.loading">Loading users...</em>-->
+        <!--<span class="text-danger" v-if="users.error">ERROR: {{users.error}}</span>-->
+        <!--<ul v-if="users.items">-->
+        <p :key="user.id" v-for="user in users.items">
+            <!--{{user.firstName + ' ' + user.lastName}}-->
+            <span v-if="user.deleting"><em>Delete user</em></span>
                 <span class="text-danger" v-else-if="user.deleteError"> - ERROR: {{user.deleteError}}</span>
-                <span v-else> - <a @click="deleteUser(user.id)" class="text-danger">Delete</a></span>
-            </li>
-        </ul>
+            <span v-else> <a @click="deleteUser(user.id)" class="text-danger">Delete myself</a></span>
+        </p>
+        <!--</ul>-->
         <p>
             <router-link to="/login">Logout</router-link>
         </p>

ce420b27442314ebc319d41d8ad26c60e1a14753
HardLight00 <imazan00@gmail.com>
2019-03-31 19:00:02 +0300

    Fix cour

--- src/surveyCreation/CheckPage.vue
+++ src/surveyCreation/CheckPage.vue
@@ -1,23 +1,23 @@
 <template>
-    <form id="example1">
-        <div >
-            <button v-on:click= "cour = cour + 2" >+1</button>
-            <p>Кнопка выше была нажата {{ cour }} раз</p>
+    <div>
+        <form>
+            <div id="example1">
+                <button v-on:click="increment(cour)">+1</button>
+                <p>Кнопка выше была нажата {{ cour }} раз</p>
 
-        </div>
-    </form>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-    import Vue from 'vue';
-
-    var sample = new Vue({
-        el: '#example1',
-        data: {
-            cour: 0
+    export default {
+        data() {
+            return {
+                cour: 1
+            };
         }
-    })
+    }
 
-
-    </script>
+</script>
 

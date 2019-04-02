db70d5289b392db587ef5b0f28a39123d7ce1b03
HardLight00 <imazan00@gmail.com>
2019-04-02 06:55:20 +0300

    Add main page

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills float-right">
+                <li class="nav-item">
+                    <router-link active-class="active" class="nav-link" exact to="/">Account</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link active-class="active" class="nav-link" to="/users">Surveys</router-link>
+                </li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">Our fancy project</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    };
+</script>
--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -3,17 +3,17 @@
         <h1>Hi {{account.user.firstName}}!</h1>
         <p>Surname: {{account.user.lastName}}</p>
         <p>Email: {{account.user.username}}</p>
-        <!--<h3>Users from secure api end point:</h3>-->
-        <!--<em v-if="users.loading">Loading users...</em>-->
-        <!--<span class="text-danger" v-if="users.error">ERROR: {{users.error}}</span>-->
-        <!--<ul v-if="users.items">-->
-        <p :key="user.id" v-for="user in users.items">
-            <!--{{user.firstName + ' ' + user.lastName}}-->
-            <span v-if="user.deleting"><em>Delete user</em></span>
+        <h3>Users from secure api end point:</h3>
+        <em v-if="users.loading">Loading users...</em>
+        <span class="text-danger" v-if="users.error">ERROR: {{users.error}}</span>
+        <ul v-if="users.items">
+            <p :key="user.id" v-for="user in users.items">
+                {{user.firstName + ' ' + user.lastName}}
+                <span v-if="user.deleting"><em>Delete user</em></span>
                 <span class="text-danger" v-else-if="user.deleteError"> - ERROR: {{user.deleteError}}</span>
-            <span v-else> <a @click="deleteUser(user.id)" class="text-danger">Delete myself</a></span>
-        </p>
-        <!--</ul>-->
+                <span v-else> <a @click="deleteUser(user.id)" class="text-danger">Delete myself</a></span>
+            </p>
+        </ul>
         <p>
             <router-link to="/login">Logout</router-link>
         </p>

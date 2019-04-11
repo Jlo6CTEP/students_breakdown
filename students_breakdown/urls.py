93b0d69bf9898a5b08bd9924338ef8969cad54fd
Ruslan <russabirov1998@mail.ru>
2019-04-11 17:16:11 +0300

    renamed branch

--- students_breakdown/urls.py
+++ students_breakdown/urls.py
@@ -19,14 +19,15 @@ from breakdown.views import UserViewSet
 from rest_framework import routers
 
 
-# Routers provide an easy way of automatically determining the URL conf.
+"""# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet"""
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
+    path('', include(breakdown.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

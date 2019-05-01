f9fc0f9ed36d919bb737415cd86f46ea350de468
Ruslan <russabirov1998@mail.ru>
2019-05-01 13:36:56 +0300

    Added admin pages for language, poll, study group, team, topic

--- breakdown/admin.py
+++ breakdown/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Survey, Course
+from .models import *
 
 
-class ProjectAdmin(admin.ModelAdmin):
+class SurveyAdmin(admin.ModelAdmin):
     list_filter = ('course_id', 'is_formed', 'due_date')
     list_display = (
         'survey_id', 'survey_name',
@@ -20,5 +20,36 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('course_id', 'name')
 
 
-admin.site.register(Survey, ProjectAdmin)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('language_id', 'language')
+    # list_display_links =
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('poll_id',
+                    'topic1', 'topic2', 'topic3',
+                    'user_id', 'course_id', 'survey_id',
+                    'language1', 'language2', 'language3',
+                    'group_by',)
+
+
+class StudyGroupAdmin(admin.ModelAdmin):
+    list_display = ('group_id', 'group')
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('team_id', 'topic_id', 'course_id')
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('topic_id', 'topic_name', 'description', 'additional_info')
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Survey, SurveyAdmin)
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(StudyGroup, StudyGroupAdmin)
+
--- breakdown/models.py
+++ breakdown/models.py
@@ -37,3 +37,65 @@ class Survey(models.Model):
         db_table = "survey"
         verbose_name = "Survey"
         verbose_name_plural = "Surveys"
+
+
+class Language(models.Model):
+    language_id = models.AutoField(primary_key=True)
+    language = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = "language"
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
+
+
+class Poll(models.Model):
+    poll_id = models.AutoField(primary_key=True)
+    topic1 = models.IntegerField()
+    topic2 = models.IntegerField()
+    topic3 = models.IntegerField()
+    user_id = models.IntegerField()
+    course_id = models.IntegerField()
+    survey_id = models.IntegerField()
+    language1 = models.IntegerField()
+    language2 = models.IntegerField()
+    language3 = models.IntegerField()
+    group_by = models.IntegerField()
+
+    class Meta:
+        db_table = "poll"
+        verbose_name = "Poll"
+        verbose_name_plural = "Polls"
+
+
+class StudyGroup(models.Model):
+    group_id = models.AutoField(primary_key=True)
+    group = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "study_group"
+        verbose_name = "Study Group"
+        verbose_name_plural = "Study Groups"
+
+
+class Topic(models.Model):
+    topic_id = models.AutoField(primary_key=True)
+    topic_name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    additional_info = models.CharField(max_length=512)
+
+    class Meta:
+        db_table = "topic"
+        verbose_name = "Topic"
+        verbose_name_plural = "Topics"
+
+
+class Team(models.Model):
+    team_id = models.IntegerField(primary_key=True)
+    topic_id = models.IntegerField()
+    course_id = models.IntegerField()
+
+    class Meta:
+        db_table = "team"
+        verbose_name = "Team"
+        verbose_name_plural = "Teams"

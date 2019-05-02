967da61a4df4af0434d8bb83199de658efb5d4a5
HardLight00 <imazan00@gmail.com>
2019-05-02 13:06:19 +0300

    Add store

--- src/_store/index.js
+++ src/_store/index.js
@@ -4,13 +4,17 @@ import Vuex from 'vuex';
 import {alert} from './alert.module';
 import {account} from './account.module';
 import {users} from './users.module';
+import {surveys} from './surveys.module';
+import {teams} from './team.module';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  modules: {
-    alert,
-    account,
-    users
-  }
+    modules: {
+        alert,
+        account,
+        users,
+        surveys,
+        teams
+    }
 });
